fd0638209520cf1d90bf0b734f658e2c3d8f06c4
Jorge Leonardo <jorgeleonardo333@gmail.com>
2023-10-08 12:21:52 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import NavBar from './components/NavBar'
+import './App.css'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  return (
+      <div className='App'>
+        <NavBar />
+        <div className="container">
+          <Outlet />
+        </div>
+      </div>
+  );
+}
+
+export default App
---
+++ src/routes/Home.jsx
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+import './Home.css'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
